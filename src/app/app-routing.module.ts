67d3bce469a863d67bedaef015e1e69ad969284a
Gabriel Rivera <xafor2@gmail.com>
2019-05-31 14:56:44 -0600

    funciona con rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/Home.component';
+import { ReporteComponent } from './reporte/reporte.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'reportes', component: ReporteComponent},
 ];
 
 @NgModule({
